f5fc3c3d5fab237360dfe6d682eec806b31dc3f1
ft-prince <ft.princeee@gmail.com>
2024-12-29 00:24:48 +0530

    Add environment variables and update project files

--- src/api/factCheckApi.js
+++ src/api/factCheckApi.js
@@ -1,12 +1,11 @@
 import axios from 'axios';
 
-const API_KEY = 'AIzaSyCOs8C_FXySEt2MUDGhB4KM1_EsQCbDmqE';
-const BASE_URL = 'https://factchecktools.googleapis.com/v1alpha1';
+
 
 export const fetchFactChecks = async (query) => {
   try {
     const response = await axios.get(
-      `${BASE_URL}/claims:search?query=${encodeURIComponent(query)}&key=${API_KEY}`
+      `${import.meta.env.VITE_BASE_URL}/claims:search?query=${encodeURIComponent(query)}&key=${import.meta.env.VITE_API_KEY}`
     );
     return response.data.claims || [];
   } catch (error) {
